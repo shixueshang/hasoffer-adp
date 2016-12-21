151161c7af84e69e7c10956384f98ff224c94640
lihongde <wy_xiaoliu@126.com>
2016-12-21 18:38:29 +0800

    sss

--- hasoffer.adp.core/src/main/java/hasoffer/adp/core/dao/SqlProvider/OrderSqlProvider.java
+++ hasoffer.adp.core/src/main/java/hasoffer/adp/core/dao/SqlProvider/OrderSqlProvider.java
@@ -1,7 +1,31 @@
-package hasoffer.adp.core.dao.SqlProvider;
+package hasoffer.adp.core.dao.sqlprovider;
+
+import org.apache.ibatis.jdbc.SQL;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 /**
  * Created by lihongde on 2016/12/19 17:37
  */
 public class OrderSqlProvider {
+
+    private final static String TABLE_NAME = "t_order";
+
+    public String getSqlByOrderTime(Map<String, Object> parameter){
+        String orderTime = (String) parameter.get("orderTime");
+
+       return new SQL()
+        {
+            {
+                SELECT("*");
+                FROM(TABLE_NAME);
+                if(!StringUtils.isEmpty(orderTime)){
+                    WHERE("date_format(orderTime, '%Y-%m-%d')='" + orderTime + "'");
+                }
+            }
+        } .toString();
+    }
+
+
 }
